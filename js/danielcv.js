7fbd7a31e57ba4cffcb15e4194d4e611ab156fb9
Daniel Cruz <Pokedann@MacBook-Pro-de-Daniel.local>
2018-05-23 22:34:51 -0500

    Mi CV version 2
    Se quitaron muchas cosas de la version 1 que no servian como la parte
    de los diplomas.
    El slider principal se dejo igual, pronto se le agregara un video.
    El perfil profesional sigue igual, solo se le cambio la frase.
    Se agrego un Timeline vertical para ahorrarnos las secciones que
    teniamos de experiencia y formacion y la del ingles, ahora todo esta en
    una sola y se ve mas sencillo.
    La seccion de los superpoderes (tecnologias) que utilizo sigue igual.
    La seccion del portafolios se hizo como un slider con varios items en
    cada slide para que no se expanda hacia abajo, asi se podra extender
    mucho mas.
    Se agrego la seccion con un slider de freelance para agregar mis
    ejercicios que hago como practica de cursos que hago.
    Se agrego la seccion de hobbies porque luego en varias entrevistas me
    preguntan cuales son mis hobbies.
    La seccion de contacto se le agregaron datos como whats y redes
    sociales, lo demas sigue igual, solo falta darle funcionalidad.
    Y al final esta el footer que sigue igual.
    Faltan muchas cosas, andamos dandole los estilos y falta la parte
    mobile.

---
+++ js/danielcv.js
@@ -0,0 +1,90 @@
+$(function(){
+    $(".sentences").typed({
+        strings: ["WEB DEVELOPER FRONT END"],
+        typeSpeed: 100,
+    backSpeed: 100
+  });
+});
+
+$( document ).ready(function() {
+    $('.tecnologias').parallax({imageSrc: 'img/tecnologias4.png'});
+    $(".rotate").textrotator({
+        animation: "flip",
+        separator: ",",
+    speed: 2000
+});
+    $('.carousel').carousel({
+        interval: 10000
+});
+    $(".test-circle").circliful({
+    animationStep: 4,
+    foregroundBorderWidth: 5,
+    backgroundBorderWidth: 15,
+    percent: 90,
+});
+    $(".test-circle1").circliful({
+    animationStep: 4,
+    foregroundBorderWidth: 5,
+    backgroundBorderWidth: 15,
+    percent: 80,
+});
+    $(".test-circle2").circliful({
+    animationStep: 4,
+    foregroundBorderWidth: 5,
+    backgroundBorderWidth: 15,
+    percent: 50,
+});
+    $(".test-circle3").circliful({
+    animationStep: 4,
+    foregroundBorderWidth: 5,
+    backgroundBorderWidth: 15,
+    percent: 50,
+});
+    $(".test-circle4").circliful({
+    animationStep: 4,
+    foregroundBorderWidth: 5,
+    backgroundBorderWidth: 15,
+    percent: 50,
+});
+    $(".test-circle5").circliful({
+    animationStep: 4,
+    foregroundBorderWidth: 5,
+    backgroundBorderWidth: 15,
+    percent: 50,
+});
+    $(".test-circle6").circliful({
+    animationStep: 4,
+    foregroundBorderWidth: 5,
+    backgroundBorderWidth: 15,
+    percent: 20,
+});
+    $(".test-circle7").circliful({
+    animationStep: 4,
+    foregroundBorderWidth: 5,
+    backgroundBorderWidth: 15,
+    percent: 80,
+});
+    $(".test-circle8").circliful({
+    animationStep: 4,
+    foregroundBorderWidth: 5,
+    backgroundBorderWidth: 15,
+    percent: 30,
+});
+    $(".test-circle9").circliful({
+    animationStep: 4,
+    foregroundBorderWidth: 5,
+    backgroundBorderWidth: 15,
+    percent: 50,
+});
+    $(".test-circle10").circliful({
+    animationStep: 4,
+    foregroundBorderWidth: 5,
+    backgroundBorderWidth: 15,
+    percent: 30,
+});
+
+// window.sr = ScrollReveal({ reset: true });
+// // Customizing a reveal set
+// sr.reveal('#carousel-example-generic, .danielTitle, .webParag, .fa-github, .fa-bitbucket-square, .fa-facebook-square, .contactBtn, .text-center, .Learning, .style-two, .danielPhoto, .profileParag, .descargarPerfil, .myProfile, .tecnologias, .test-circle, .test-circle1, .test-circle2, .test-circle3, .test-circle4, .test-circle5, .test-circle6, .portfolio, .simplefilter, .search-row, .style-twos, .panel-default, .img-rounded, .timeline-panel, .fa-briefcase, .style-four, .blanco, .timeline-badge, .fa-globe, .ingles, .rotate, .progress-bar, .glyphicon-check, .margen, .miContacto, .contactTitle, .contactSubtitle, label, input, .divider, textarea', 
+//     { duration: 1300});
+});
