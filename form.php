6badda1cb860c1e1063c18cb921799bb3ea3d699
Daniel Cruz <Pokedann@MacBook-Pro-de-Daniel.local>
2018-05-29 19:01:55 -0500

    No recaptcha y pagina de gracias
    No se pudo lograr lo del recaptcha, si se pudo implementar en el
    formulario pero se probaron varios codigos para que no se pudiera
    apretar el boton de enviar hasta que se llene el recaptcha en js y php
    y no funcionaron, se quita lo del recaptcha y el formulario queda
    normal.
    Se agrego una pagina de gracias para cuando se llena el formulario,
    despues de 15 segundos te redirige a la principal

--- form.php
+++ form.php
@@ -5,13 +5,13 @@ $Vacante = $_POST['vacante'];
 $Correo = $_POST['email'];
 $Mensaje = $_POST['descripcion'];
 
-$secret = "6LdkqVsUAAAAABQS0mfH6b7nSsFH6Jrt6wAn4rzz";
-$response = $_POST["g-recaptcha-response"];
-$url = "https://www.google.com/recaptcha/api/siteverify";
-$ip = $_SERVER['REMOTE_ADDR'];
-$finalResponse = file_get_contents($url."?secret=".$secret."&response=".$response."&remoteip=".$ip);
+// $secret = "6LdkqVsUAAAAABQS0mfH6b7nSsFH6Jrt6wAn4rzz";
+// $response = $_POST["g-recaptcha-response"];
+// $url = "https://www.google.com/recaptcha/api/siteverify";
+// $ip = $_SERVER['REMOTE_ADDR'];
+// $finalResponse = file_get_contents($url."?secret=".$secret."&response=".$response."&remoteip=".$ip);
 
-$header = "From: ".$_POST["nombre"]."<"$_POST["email"].">" . " \r\n";
+$header = "From: ". $_POST["nombre"] . "<" . $_POST["email"] . ">" . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
@@ -39,7 +39,6 @@ $mensaje = '<html>'.
 	'<p style="font-size: 18px;color: #00c0ff;">La vacante que tiene es: ' . $Vacante . ",\r\n". '</p>'.
 	'<p style="font-size: 18px;color: #00c0ff;">Su e-mail es: ' . $Correo . ", \r\n". '</p>'.
 	'<p style="font-size: 18px;color: #00c0ff;">El mensaje que dejo es: ' . $_POST['descripcion'] . ", \r\n". '</p>'.
-	'<p style="font-size: 18px;color: #00c0ff;">Los datos del captcha son: ' . $finalResponse . ", \r\n". '</p>'.
 	'<p style="font-size: 18px;color: #00c0ff;">Enviado el ' . date('d/m/Y', time()). '</p>'.
 	// '<hr>'.
 	// 'Enviado por mi programa en PHP'. //aqui seria como una firma
@@ -48,9 +47,9 @@ $mensaje = '<html>'.
 
 $mensajeDos = '<html>'.
 	'<head><title></title></head>'.
-	'<body><h3>Muchas gracias por contactarme </h3>'. $Nombre . ":".
-	'<img style="width: 160px;" src="https://cdn.bulbagarden.net/upload/thumb/5/5b/Ilima_Eevee.png/250px-Ilima_Eevee.png"><br>'.
-	'<p style="font-size: 18px;color: red;">Pronto me pondre en contacto contigo para concertar una entrevista, por lo mientras te invito a seguir revisando mi portafolios en http://danielcruz.esy.es/ ' . ",\r\n". '</p>'.
+	'<body><h2>Muchas gracias por contactarme '. $Nombre . ':</h2>'.
+	'<p style="font-size: 16px;color: #03A9F4;">Pronto me pondre en contacto contigo para concertar una entrevista, mientras tanto te invito a seguir revisando mi portafolios en http://danielcruz.esy.es/ ' . "\r\n". '</p>'.
+	'<img style="width: 250px;margin: 0 auto; display: block;" src="https://pbs.twimg.com/media/DCFqHLxUAAAhadB.jpg"><br>'.
 	'</body>'.
 	'</html>';
 
@@ -61,5 +60,5 @@ $asunto = 'Mensaje de Daniel Cruz CV';
 mail($para, $asunto, utf8_decode($mensaje), $header);
 mail($paraDos, $asunto, utf8_decode($mensajeDos), $headerDos);
 
-header("Location:index.html");
+header("Location:gracias.php");
  ?>
---
+++ gracias.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link href="css/gracias.css" rel="stylesheet">
+	<title>Gracias</title>
+</head>
+<body class="thanks">
+	<?php 
+		header( "refresh:15;url=/" );
+	 ?>
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 text-center">
+					<img src="img/gracias.jpg" class="imgGracias">
+					<p><span class="interes">POR TU INTERÉS</span></p>
+					<h3 class="recibido">He recibido satisfactoriamente tu mensaje</h3>
+					<p class="revisar">Te invito a revisar tu correo para leer el mensaje de confirmacion que te he enviado</p>
+					<p>Pronto seras redirigido a la pagina principal, si no te redirige en 15 segundos dale clic <a href="/"><u>aqui:</u></a></p>
+				</div>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
